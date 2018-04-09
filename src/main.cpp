318f1a473a70d6b5509496f9de46402b958a64d2
Chris Brammer <faauxtv@gmail.com>
2018-04-09 11:09:20 +0200

    Cross Platform SDL

--- src/main.cpp
+++ src/main.cpp
@@ -168,7 +168,7 @@ int main(int, char*[])
         SDL_QueryTexture(texture, NULL, NULL, &w, &h);
 
         SDL_LockTexture(texture, nullptr, &pixels, &pitch);
-        memcpy(pixels, image.data(), image.size() * sizeof(u8));
+        std::memcpy(pixels, image.data(), image.size() * sizeof(u8));
         SDL_UnlockTexture(texture);
 
         // Render to screen
